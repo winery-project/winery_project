00f0512dbfd8fd96e714ab762b46df173627061e
Kii120 <117469095+Kii120@users.noreply.github.com>
2024-02-21 14:18:59 +0900

    finish genre features

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,49 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
+
   def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "You have created the genre successfully."
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "You have updated the genre successfully."
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    genre = Genre.find(params[:id])
+    if genre.destroy
+      flash[:notice] = "You have deleted the genre successfully."
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
